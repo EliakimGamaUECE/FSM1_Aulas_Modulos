11d67af04ab92ebb808bca2ed23d1316fed62588
EliakimGama <eliakimgama@hotmail.com>
2025-05-24 23:04:50 -0300

    Aulas 01 e 02

---
+++ 02_MODULO_INTERMADIARIO/FSM1_AULA_02/script.js
@@ -0,0 +1,13 @@
+// OPERADORES EM JAVASCRIPT
+
+// ARITMÉTICOS: (+  -  *  /  %  **  ++  --)
+// RELACIONAIS: (==  !=  ===  !==  >  <  >=  <=)
+// LÓGICOS    : (&&  ||  !)
+// TERNÁRIO   : (? :)
+
+var num1 = 13
+var num2 = 13
+
+var res = num1 > num2?"É maior":"É menor"
+
+document.writeln(res)
